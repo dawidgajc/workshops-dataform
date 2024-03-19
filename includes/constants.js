67f4d5a5d46174dbc7c2c862f019c86f39741e0a
dawidgajc <dawid.gajc@devoteam.com>
2024-03-19 14:32:33 +0100

    taxi dataset

--- includes/constants.js
+++ includes/constants.js
@@ -1,8 +1,10 @@
 const project = "tomasz-m-sandbox";
 const tableSchema = "dataform";
 
+const sourceTable = `bigquery-public-data.chicago_taxi_trips.taxi_trips`;
 
 module.exports = {
     project,
-    tableSchema
+    tableSchema,
+    sourceTable
 };
--- includes/docs.js
+++ includes/docs.js
@@ -1,25 +1,33 @@
 // Docs for the columns in the incremental tables:
-const aggregated_columns = {
-    billing_account_id: billing_account_id,
-    customer_correlation_id: customer_correlation_id,
-    sku_id: sku_id,
-    sku_description: sku_description,
-    global_cost: global_cost,
-    invoice_month: invoice_month,
-    usage_in_pricing_unit: usage_in_pricing_unit,
-    pricing_unit: pricing_unit
-}
+const taxi_columns = {
+    unique_key: "Unique identifier for the trip.",
+    taxi_id: "A unique identifier for the taxi.",
+    trip_start_timestamp: "When the trip started, rounded to the nearest 15 minutes.",
+    trip_start_timestamp: "When the trip ended, rounded to the nearest 15 minutes.",
+    trip_seconds: "Time of the trip in seconds.",
+    trip_miles: "Distance of the trip in miles.",
+    pickup_census_tract: "The Census Tract where the trip began. For privacy, this Census Tract is not shown for some trips.",
+    dropoff_census_tract: "The Census Tract where the trip ended. For privacy, this Census Tract is not shown for some trips.",
+    pickup_community_area: "The Community Area where the trip began.",
+    dropoff_community_area: "The Community Area where the trip ended.",
+    fare: "The fare for the trip.",
+    tips: "The tip for the trip. Cash tips generally will not be recorded.",
+    tolls: "The tolls for the trip.",
+    extras: "Extra charges for the trip.",
+    trip_total: "Total cost of the trip, the total of the fare, tips, tolls, and extras.",
+    payment_type: "Type of payment for the trip.",
+    company: "The taxi company.",
+    pickup_latitude: "The latitude of the center of the pickup census tract or the community area if the census tract has been hidden for privacy.",
+    pickup_longitude: "The longitude of the center of the pickup census tract or the community area if the census tract has been hidden for privacy.",
+    pickup_location: "The location of the center of the pickup census tract or the community area if the census tract has been hidden for privacy.",
+    dropoff_latitude: "The latitude of the center of the dropoff census tract or the community area if the census tract has been hidden for privacy.",
+    dropoff_longitude: "The longitude of the center of the dropoff census tract or the community area if the census tract has been hidden for privacy.",
+    dropoff_location: "	The location of the center of the dropoff census tract or the community area if the census tract has been hidden for privacy."
 
-// Docs for the columns in the monthly tables:
-const country = "Owner of rebilling dataset";
-const service_id = "The ID of the service that the usage is associated with.";
-const service_description = "The Google service that reported the billing data";
-const project_name = "The name of the Google Cloud project that generated the Cloud Billing data.";
-const project_id = "The ID of the Google Cloud project that generated the Cloud Billing data.";
+
+}
 
 
 module.exports = {
     aggregated_columns,
-    country,
-    service_id
 };
